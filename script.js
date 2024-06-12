36610a370e190c8327e5afdd96da04a928ff8ccd
ahmedifrah <ahmed.ifrah.ibrahim@gmail.com>
2024-06-12 16:16:47 +0500

    check dynamically if player is won

--- script.js
+++ script.js
@@ -30,11 +30,31 @@ const gameBoard = (() =>{
 })();
 
 const gameController = (() =>{
-    const isX = (sign) => sign == "X"
+    const isX = (sign) => sign === "X"
+    let haveWinner = false
     const winCondition = (gameBoard) => {
-        // return gameBoard.board[0].every(isX)
-        let first_col =  gameBoard.board.map(x => x[0])
-        return first_col.every(isX)
+        // for (let k = 0; k < gameBoard.length; k++){
+        //     gameBoard.board[0].every(isX)
+        // }
+        for (let k = 0; k < gameBoard.board.length; k++){
+
+            let columnCheck = gameBoard.board.map(x => x[k])
+            let diagonal = getMainDiagonal(gameBoard.board)
+            let backDiagonal = getAntiDiagonal(gameBoard.board)
+
+            if (gameBoard.board[k].every(isX) == true || columnCheck.every(isX) == true || diagonal.every(isX) == true || backDiagonal.every(isX)){
+                haveWinner = true;
+            }
+        }
+        return haveWinner
+        
+        // for (k = 0; k < gameBoard.length; k++){
+        //     var won = gameBoard.board[k].every(isX("X"))
+        //     console.log(won)
+
+        // }
+        // let first_col =  gameBoard.board.map(x => x[0])
+        // return first_col.every(isX)
 
     }
     
@@ -55,19 +75,21 @@ const getAntiDiagonal = (board) => {
     for (let i = 0; i < board.length; i++){
         antiDiagonal.push(board[i][board.length - 1 - i])
     }
+    return antiDiagonal
 }
-
-
-
-
-
-
 gameBoard.addSign("X", 0, 0)
-gameBoard.addSign("X", 0, 1)
+
 gameBoard.addSign("X", 0, 2)
+
+
 gameBoard.addSign("X", 1, 0)
-gameBoard.addSign("X", 2, 0)
+gameBoard.addSign("O", 2, 0)
+gameBoard.addSign("X", 1, 1)
+gameBoard.addSign("0", 2, 2)
 
 console.log(gameBoard.board)
 console.log(gameController.winCondition(gameBoard))
-console.log(getMainDiagonal(gameBoard.board))
+
+
+
+
